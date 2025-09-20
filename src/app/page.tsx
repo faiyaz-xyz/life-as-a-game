99e836f4e4bbcd39411c26d5aac08a454d21b96b
faiyaz-xyz <tahiat0967@gmail.com>
2025-09-20 11:09:50 +0600

    Made routes protected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import DailyTasks from "@/components/daily-tasks";
+import { useAuth } from "@/components/useAuth";
+
+export default function HomePage() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login"); // redirect if not logged in
+    }
+  }, [user, loading, router]);
+
+  if (loading) return <div>Loading...</div>; // show loading while checking auth
+
+  return <>{user && <DailyTasks />}</>; // only show tasks if logged in
 }
